c5b5b6bccb15b57f2901f51fb79c37e29ad32ba2
mariamyusra21 <79508936+mariamyusra21@users.noreply.github.com>
2022-08-06 22:25:32 +0500

    Student Added to the Database Operation is completed.Update README.md

--- FirstDatabase/StudentInfoForm.cs
+++ FirstDatabase/StudentInfoForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Configuration;
@@ -28,25 +29,30 @@ namespace FirstDatabase
                 
                 using (SqlConnection connection = new SqlConnection(connString))
                 {
-                    using (SqlCommand sqlCommand = new SqlCommand())
+                    connection.Open();
+                    string query = "INSERT INTO StudentInfo(Name, Email) VALUES(@Name,@Email)";
+                    using (SqlCommand sqlCommand = new SqlCommand(query,connection))
                     {
-                        sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                        // sqlCommand.CommandType = CommandType.StoredProcedure;
 
-                        //Parameters for StudentInsetDetails
+                        // Parameters for StudentInsetDetails
 
-                        sqlCommand.Parameters.Add("@Name", System.Data.SqlDbType.NVarChar, 100).Value = NameTextBox.Text;
-                        sqlCommand.Parameters.AddWithValue("Email", EmailTextBox.Text);
+                        // sqlCommand.Parameters.Add("@Name", SqlDbType.NVarChar, 100).Value = NameTextBox.Text;
+                        sqlCommand.Parameters.AddWithValue("@Name", NameTextBox.Text);
+                        sqlCommand.Parameters.AddWithValue("@Email", EmailTextBox.Text);
 
                         // Open the database connection
-                        connection.Open();
+                        // connection.Open();
 
                         /*  ExecuteReader   =>>>    For Select Statements.
                          *  ExecuteScalar   =>>>    For Select Statements.
                          *  ExecuteNonQuery  =>>>    For Insert, Updat, and Delete Statements.
-                         */
+                        */
 
                         // Execute Insert Statement
                         sqlCommand.ExecuteNonQuery();
+
+                        connection.Close();
 
                         MessageBox.Show("Studnet Details are added to the System", "User Added", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
