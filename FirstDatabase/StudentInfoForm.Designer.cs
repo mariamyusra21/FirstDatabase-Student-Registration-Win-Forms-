9502670d8562cd0d5fe7281ed6b0808114b6f615
Mariam_Yusra21 <memon.yusra21@gmail.com>
2022-08-18 21:18:09 +0500

    Added Phone Number functionality.

--- FirstDatabase/StudentInfoForm.Designer.cs
+++ FirstDatabase/StudentInfoForm.Designer.cs
@@ -32,10 +32,17 @@
             this.NameTextBox = new System.Windows.Forms.TextBox();
             this.EmailLabel = new System.Windows.Forms.Label();
             this.EmailTextBox = new System.Windows.Forms.TextBox();
-            this.SaveButton = new System.Windows.Forms.Button();
             this.CloseButton = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.InterestLabel = new System.Windows.Forms.Label();
+            this.IsCSharpCheckBox = new System.Windows.Forms.CheckBox();
+            this.IsFlutterCheckBox = new System.Windows.Forms.CheckBox();
+            this.IsJavaCheckBox = new System.Windows.Forms.CheckBox();
+            this.PlatformLabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.PhNumberLabel = new System.Windows.Forms.Label();
+            this.PhNumberTextBox = new System.Windows.Forms.TextBox();
+            this.PhValidatorButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // NameLabel
@@ -76,60 +83,139 @@
             this.EmailTextBox.Size = new System.Drawing.Size(357, 31);
             this.EmailTextBox.TabIndex = 1;
             // 
-            // SaveButton
-            // 
-            this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SaveButton.Location = new System.Drawing.Point(149, 268);
-            this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(132, 41);
-            this.SaveButton.TabIndex = 2;
-            this.SaveButton.Text = "Save";
-            this.SaveButton.UseVisualStyleBackColor = true;
-            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
-            // 
             // CloseButton
             // 
             this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CloseButton.Location = new System.Drawing.Point(367, 268);
+            this.CloseButton.Location = new System.Drawing.Point(374, 394);
             this.CloseButton.Name = "CloseButton";
-            this.CloseButton.Size = new System.Drawing.Size(132, 41);
+            this.CloseButton.Size = new System.Drawing.Size(132, 31);
             this.CloseButton.TabIndex = 3;
             this.CloseButton.Text = "Close";
             this.CloseButton.UseVisualStyleBackColor = true;
             this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
             // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(367, 268);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(132, 41);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Close";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.CloseButton_Click);
-            // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(149, 268);
+            this.button1.Location = new System.Drawing.Point(165, 394);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 41);
+            this.button1.Size = new System.Drawing.Size(132, 31);
             this.button1.TabIndex = 2;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.SaveButton_Click);
             // 
+            // InterestLabel
+            // 
+            this.InterestLabel.AutoSize = true;
+            this.InterestLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InterestLabel.Location = new System.Drawing.Point(55, 268);
+            this.InterestLabel.Name = "InterestLabel";
+            this.InterestLabel.Size = new System.Drawing.Size(104, 17);
+            this.InterestLabel.TabIndex = 0;
+            this.InterestLabel.Text = "Interested In:";
+            // 
+            // IsCSharpCheckBox
+            // 
+            this.IsCSharpCheckBox.AutoSize = true;
+            this.IsCSharpCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IsCSharpCheckBox.Location = new System.Drawing.Point(180, 269);
+            this.IsCSharpCheckBox.Name = "IsCSharpCheckBox";
+            this.IsCSharpCheckBox.Size = new System.Drawing.Size(46, 21);
+            this.IsCSharpCheckBox.TabIndex = 4;
+            this.IsCSharpCheckBox.Text = "C#";
+            this.IsCSharpCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // IsFlutterCheckBox
+            // 
+            this.IsFlutterCheckBox.AutoSize = true;
+            this.IsFlutterCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IsFlutterCheckBox.Location = new System.Drawing.Point(302, 269);
+            this.IsFlutterCheckBox.Name = "IsFlutterCheckBox";
+            this.IsFlutterCheckBox.Size = new System.Drawing.Size(74, 21);
+            this.IsFlutterCheckBox.TabIndex = 5;
+            this.IsFlutterCheckBox.Text = "Flutter";
+            this.IsFlutterCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // IsJavaCheckBox
+            // 
+            this.IsJavaCheckBox.AutoSize = true;
+            this.IsJavaCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IsJavaCheckBox.Location = new System.Drawing.Point(435, 268);
+            this.IsJavaCheckBox.Name = "IsJavaCheckBox";
+            this.IsJavaCheckBox.Size = new System.Drawing.Size(61, 21);
+            this.IsJavaCheckBox.TabIndex = 6;
+            this.IsJavaCheckBox.Text = "Java";
+            this.IsJavaCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // PlatformLabel
+            // 
+            this.PlatformLabel.AutoSize = true;
+            this.PlatformLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlatformLabel.Location = new System.Drawing.Point(55, 318);
+            this.PlatformLabel.Name = "PlatformLabel";
+            this.PlatformLabel.Size = new System.Drawing.Size(81, 17);
+            this.PlatformLabel.TabIndex = 0;
+            this.PlatformLabel.Text = "Platforms:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(165, 318);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(341, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // PhNumberLabel
+            // 
+            this.PhNumberLabel.AutoSize = true;
+            this.PhNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PhNumberLabel.Location = new System.Drawing.Point(47, 149);
+            this.PhNumberLabel.Name = "PhNumberLabel";
+            this.PhNumberLabel.Size = new System.Drawing.Size(50, 20);
+            this.PhNumberLabel.TabIndex = 0;
+            this.PhNumberLabel.Text = "Ph #:";
+            // 
+            // PhNumberTextBox
+            // 
+            this.PhNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PhNumberTextBox.Location = new System.Drawing.Point(149, 138);
+            this.PhNumberTextBox.MaxLength = 11;
+            this.PhNumberTextBox.Multiline = true;
+            this.PhNumberTextBox.Name = "PhNumberTextBox";
+            this.PhNumberTextBox.Size = new System.Drawing.Size(357, 31);
+            this.PhNumberTextBox.TabIndex = 1;
+            this.PhNumberTextBox.TextChanged += new System.EventHandler(this.PhNumberTextBox_TextChanged);
+            this.PhNumberTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PhNumberTextBox_KeyPress);
+            // 
+            // PhValidatorButton
+            // 
+            this.PhValidatorButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PhValidatorButton.Location = new System.Drawing.Point(543, 138);
+            this.PhValidatorButton.Name = "PhValidatorButton";
+            this.PhValidatorButton.Size = new System.Drawing.Size(132, 31);
+            this.PhValidatorButton.TabIndex = 3;
+            this.PhValidatorButton.Text = "Validate Ph #";
+            this.PhValidatorButton.UseVisualStyleBackColor = true;
+            this.PhValidatorButton.Click += new System.EventHandler(this.PhValidatorButton_Click);
+            // 
             // StudentInfoForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(590, 359);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(707, 463);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.IsJavaCheckBox);
+            this.Controls.Add(this.IsFlutterCheckBox);
+            this.Controls.Add(this.IsCSharpCheckBox);
+            this.Controls.Add(this.PhValidatorButton);
             this.Controls.Add(this.CloseButton);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.PhNumberTextBox);
             this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.PlatformLabel);
+            this.Controls.Add(this.InterestLabel);
+            this.Controls.Add(this.PhNumberLabel);
             this.Controls.Add(this.EmailLabel);
             this.Controls.Add(this.NameTextBox);
             this.Controls.Add(this.NameLabel);
@@ -149,9 +235,16 @@
         private System.Windows.Forms.TextBox NameTextBox;
         private System.Windows.Forms.Label EmailLabel;
         private System.Windows.Forms.TextBox EmailTextBox;
-        private System.Windows.Forms.Button SaveButton;
         private System.Windows.Forms.Button CloseButton;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label InterestLabel;
+        private System.Windows.Forms.CheckBox IsCSharpCheckBox;
+        private System.Windows.Forms.CheckBox IsFlutterCheckBox;
+        private System.Windows.Forms.CheckBox IsJavaCheckBox;
+        private System.Windows.Forms.Label PlatformLabel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label PhNumberLabel;
+        private System.Windows.Forms.TextBox PhNumberTextBox;
+        private System.Windows.Forms.Button PhValidatorButton;
     }
 }
--- FirstDatabase/StudentInfoForm.cs
+++ FirstDatabase/StudentInfoForm.cs
@@ -29,40 +29,87 @@ namespace FirstDatabase
 
             using (SqlConnection connection = new SqlConnection(connString))
             {
-                connection.Open();
-
-                //string query = "INSERT INTO StudentInfo(Name, Email) VALUES(@Name,@Email)";
-
-                string storedproc = @"dbo.[userproc_StudentInsertDetail]";
-
-                using (SqlCommand sqlCommand = new SqlCommand(storedproc, connection))
+                if (PhNumberTextBox.TextLength == 11)
                 {
-                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
 
-                    // Parameters for StudentInsetDetails
+                    //string query = "INSERT INTO StudentInfo(Name, Email) VALUES(@Name,@Email)";
 
-                    // sqlCommand.Parameters.Add("@Name", SqlDbType.NVarChar, 100).Value = NameTextBox.Text;
-                    sqlCommand.Parameters.AddWithValue("@Name", NameTextBox.Text);
-                    sqlCommand.Parameters.AddWithValue("@Email", EmailTextBox.Text);
+                    string storedInsertProc = @"dbo.[userproc_StudentInsertDetail]";
 
-                    // Open the database connection
-                    // connection.Open();
+                    using (SqlCommand sqlCommand = new SqlCommand(storedInsertProc, connection))
+                    {
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
+                       
+                        // Parameters for StudentInsetDetails
 
-                    /*  ExecuteReader   =>>>    For Select Statements.
-                     *  ExecuteScalar   =>>>    For Select Statements.
-                     *  ExecuteNonQuery  =>>>    For Insert, Updat, and Delete Statements.
-                    */
+                        // sqlCommand.Parameters.Add("@Name", SqlDbType.NVarChar, 100).Value = NameTextBox.Text;
+                        sqlCommand.Parameters.AddWithValue("@Name", NameTextBox.Text);
+                        sqlCommand.Parameters.AddWithValue("@Email", EmailTextBox.Text);
+                        sqlCommand.Parameters.AddWithValue("@PhoneNumber", PhNumberTextBox.Text);
+                        sqlCommand.Parameters.AddWithValue("@IsInterestedInCSharp", IsCSharpCheckBox.Checked);
+                        sqlCommand.Parameters.AddWithValue("@IsInterestedInFlutter", IsFlutterCheckBox.Checked);
+                        sqlCommand.Parameters.AddWithValue("@IsInterestedInJava", IsJavaCheckBox.Checked);
 
-                    // Execute Insert Statement
-                    sqlCommand.ExecuteNonQuery();
 
-                    connection.Close();
+                        // Open the database connection
+                        // connection.Open();
 
-                    MessageBox.Show("Studnet Details are added to the System", "User Added", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        /*  ExecuteReader   =>>>    For Select Statements.
+                         *  ExecuteScalar   =>>>    For Select Statements.
+                         *  ExecuteNonQuery  =>>>    For Insert, Updat, and Delete Statements.
+                        */
+
+                        // Execute Insert Statement
+                        sqlCommand.ExecuteNonQuery();
+
+                        connection.Close();
+                    }
                 }
-            }
+                else
+                {
+                    MessageBox.Show("Phone Number is Mandatory and it should contains 11 Numeric characters.", "Student details are Incomplete", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                }
 
+                MessageBox.Show("Studnet Details are added to the System", "User Added", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
+
+        private void PhNumberTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Error! A phone Number cannot contains letters./nPlease Enter 11 digit Number.", "Enter A Valid Phone Number", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void PhNumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (PhNumberTextBox.TextLength == 11)
+            {
+                PhNumberTextBox.ForeColor = Color.Black;
+            }
+            else
+            {
+                PhNumberTextBox.ForeColor = Color.Red;
+            }
+        }
+
+        private void PhValidatorButton_Click(object sender, EventArgs e)
+        {
+            if (PhNumberTextBox.TextLength == 11)
+            {
+                MessageBox.Show("Phone Number is successfully validated.");
+            }
+            else
+            {
+                MessageBox.Show("Phone Number should contain 11 digits.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
     }
+
 }
